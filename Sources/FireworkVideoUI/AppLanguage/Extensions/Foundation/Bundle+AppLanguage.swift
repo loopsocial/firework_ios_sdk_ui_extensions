5ee9065b84262fc6bbde378a6f396886866ab0c8
Jayden <linjie@fireworkhq.com>
2023-06-06 11:10:40 +0800

    Update Bundle+AppLanguage.swift

--- Sources/FireworkVideoUI/AppLanguage/Extensions/Foundation/Bundle+AppLanguage.swift
+++ Sources/FireworkVideoUI/AppLanguage/Extensions/Foundation/Bundle+AppLanguage.swift
@@ -24,21 +24,18 @@ extension Bundle {
            let languageCode = AppLanguageManager.shared.appLanguageCode,
            Bundle(for: FireworkVideoSDK.self) == self {
             let iOSSDKBundle = Bundle(for: FireworkVideoSDK.self)
-
             var languageBundlePath: String?
             let defaultLanguageBundlePath = iOSSDKBundle.path(forResource: "Base", ofType: "lproj")
-            if languageCode == "en" {
-                languageBundlePath = defaultLanguageBundlePath
-            } else if let path = iOSSDKBundle.path(forResource: language, ofType: "lproj") {
+            if let path = iOSSDKBundle.path(forResource: language, ofType: "lproj") {
                 languageBundlePath = path
             } else if let path = iOSSDKBundle.path(forResource: languageCode, ofType: "lproj") {
                 languageBundlePath = path
             } else {
                 let targeLanguageList = iOSSDKBundle.localizations.filter { item in
-                    return item != "Base" && item != "en"
+                    return item != "Base"
                 }
                 if let targeLanguage = targeLanguageList.first(where: { item in
-                    let targeLanguageCode = LanguageExtension.getLanguageCode(item)
+                    let targeLanguageCode = LanguageUtil.getLanguageCode(item)
                     return languageCode == targeLanguageCode
                 }) {
                     languageBundlePath = iOSSDKBundle.path(forResource: targeLanguage, ofType: "lproj")
@@ -47,7 +44,19 @@ extension Bundle {
 
             if let resultLanguageBundlePath = languageBundlePath ?? defaultLanguageBundlePath,
                let resultLanguageBundle = Bundle(path: resultLanguageBundlePath) {
-                return resultLanguageBundle.fw_localizedString(forKey: key, value: value, table: tableName)
+                let localizedString = resultLanguageBundle.fw_localizedString(forKey: key, value: value, table: tableName)
+                var keyNotFound = false
+                if (value ?? "").isEmpty && localizedString == key {
+                    keyNotFound = true
+                } else if (value ?? "").count > 0 && localizedString == value {
+                    keyNotFound = true
+                }
+                if keyNotFound, resultLanguageBundlePath != defaultLanguageBundlePath {
+                    let defaultLanguageBundle = Bundle(path: defaultLanguageBundlePath ?? "")
+                    return defaultLanguageBundle?.fw_localizedString(forKey: key, value: value, table: tableName) ?? localizedString
+                } else {
+                    return localizedString
+                }
             } else {
                 return self.fw_localizedString(forKey: key, value: value, table: tableName)
             }
